c4a1530b079596dee0597d8c184966ddd5d34672
Davixxa <serverboost100.dk@gmail.com>
2022-10-11 14:37:42 +0200

    dumbass placeholder shit

--- UniCityBuilder/src/Main.java
+++ UniCityBuilder/src/Main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // Initial program
         // Aleksander
         System.out.println("**** Start apprication ****");
+        System.out.println("Enter name: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Name: " + name);
     }
 
 }

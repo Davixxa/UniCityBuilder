cfa97e9c6586af81a5ac4e5bb48264d3f212f3b9
Marcus Boel Quebec Jürgensen <mbqj@hotmail.dk>
2022-11-01 09:38:20 +0100

    Polution manager and starting on main

--- UniCityBuilder/src/Main.java
+++ UniCityBuilder/src/Main.java
@@ -11,6 +11,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         System.out.println("Name: " + name);
+        
+        InfastructureManager infastructureManager = new InfastructureManager();
+        BuildingManager buildingManager = new BuildingManager();
+        MoneyManager moneyManager = new MoneyManager(buildingManager);
+        EnergyManager energyManager = new EnergyManager(buildingManager);
+        ScienceManager scienceManager = new ScienceManager(buildingManager);
+        DisasterManager disasterManager = new DisasterManager(buildingManager);
     }
 }
 
---
+++ UniCityBuilder/src/PolutionManager.java
@@ -0,0 +1,24 @@
+public class PolutionManager {
+    private int currentPolution;
+    private  int polutionYield;
+
+    public PolutionManager(){
+        currentPolution = 0;
+    }
+
+    public void setPolutionYield(int polutionYield) {
+        this.polutionYield = polutionYield;
+    }
+
+    public int getCurrentPolution() {
+        return currentPolution;
+    }
+
+    public void setCurrentPolution(int currentPolution) {
+        this.currentPolution = currentPolution;
+    }
+
+    public int getPolutionYield() {
+        return polutionYield;
+    }
+}

fcfc36ff9782266d1a5f1ee91d559e9cf0f8a627
Oliver Larsen <Oliver1703dk@hotmail.com>
2022-11-22 13:26:26 +0100

    Aleksander

--- UniCityBuilderGUI/src/main/java/com/example/unicitybuildergui/HelloController.java
+++ UniCityBuilderGUI/src/main/java/com/example/unicitybuildergui/HelloController.java
@@ -1,14 +1,24 @@
 package com.example.unicitybuildergui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button button1;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    public void initialize() {
+        //imageView = new ImageView(getClass().getResource("Arrow.png").toExternalForm());
+        //button1.setGraphic(imageView);
+
     }
+
 }

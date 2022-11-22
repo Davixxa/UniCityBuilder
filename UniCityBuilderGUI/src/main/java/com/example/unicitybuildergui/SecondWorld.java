6634876e180bd143c720e307b04cabf66e6cd8ca
Oliver Larsen <Oliver1703dk@hotmail.com>
2022-11-22 13:57:49 +0100

    Aleksander

--- UniCityBuilderGUI/src/main/java/com/example/unicitybuildergui/SecondWorld.java
+++ UniCityBuilderGUI/src/main/java/com/example/unicitybuildergui/SecondWorld.java
@@ -7,17 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class SecondWorld extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Untitled.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(SecondWorld.class.getResource("Untitled.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 750, 750);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 }

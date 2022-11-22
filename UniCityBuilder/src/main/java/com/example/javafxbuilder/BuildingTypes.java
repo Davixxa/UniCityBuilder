85120c312a13d231954ee00d5137ee17636e904a
Oliver Larsen <Oliver1703dk@hotmail.com>
2022-11-22 11:55:17 +0100

    New project because this shit doesnt work

--- UniCityBuilder/src/main/java/com/example/javafxbuilder/BuildingTypes.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.example.javafxbuilder;
-
-public enum BuildingTypes {
-    MONEY,
-    SCIENCE,
-    ENERGY,
-    HOUSE
-}
--- UniCityBuilder/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javafxbuilder {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javafxbuilder to javafx.fxml;
-    exports com.example.javafxbuilder;
-}
